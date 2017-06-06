7f8c0e005c538b0e7b70dff0907a790a3d52a8b2
bmac423 <bmac423@gmail.com>
2017-06-06 16:20:06 +0000

    added motd

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -1,10 +1,10 @@
 name 'apache'
-maintainer 'Brandin'
+maintainer 'Brandon'
 maintainer_email 'brandon@linuxacademy.com'
 license 'all_rights'
 description 'Installs/Configures apache'
 long_description 'Installs/Configures apache'
-version '0.1.0'
+version '0.2.1'
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
